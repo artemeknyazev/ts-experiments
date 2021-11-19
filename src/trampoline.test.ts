6a54dd538e2bd98c80531e8eda02556b66c6b5ee
artem.e.knyazev <artem.e.knyazev@yandex.ru>
2021-11-19 13:12:27 +0300

    More generic trampoline

--- src/trampoline.test.ts
+++ src/trampoline.test.ts
@@ -1,15 +1,20 @@
 import * as E from "fp-ts/lib/Either";
-import { transformTrampoline, RecFunc } from "./trampoline";
+import {
+  trampolineORSR,
+  TrampolinedORSR,
+  trampolineOR,
+  TrampolinedOR,
+} from "./trampoline";
 
-describe("transformTrampoline", () => {
+describe("trampolineORSR", () => {
   it("transforms trampolined standard form factorial correctly", () => {
     // A recursive factorial function in a standard form
     const f = (n: number): number => (n > 1 ? n * f(n - 1) : 1);
 
     // A trampolined standard form factorial function
-    const ftr_: RecFunc<[number], number> = (n) =>
+    const ftr_: TrampolinedORSR<[number], number> = (n) =>
       n > 1 ? E.left([(x) => n * x, [n - 1]]) : E.right(1);
-    const ftr = transformTrampoline(ftr_);
+    const ftr = trampolineORSR(ftr_);
 
     for (let i = 0; i < 10; i++) {
       expect(ftr(i)).toBe(f(i));
@@ -24,10 +29,10 @@ describe("transformTrampoline", () => {
     const f = (n: number) => f_(n, 1);
 
     // A trampolined tail-call form factorial function
-    const ftr_: RecFunc<[number, number], number> = (n, acc) =>
+    const ftr_: TrampolinedORSR<[number, number], number> = (n, acc) =>
       n > 1 ? E.left([undefined, [n - 1, n * acc]]) : E.right(acc);
 
-    const ftr = (n: number) => transformTrampoline(ftr_)(n, 1);
+    const ftr = (n: number) => trampolineORSR(ftr_)(n, 1);
 
     for (let i = 0; i < 10; i++) {
       expect(ftr(i)).toBe(f(i));
@@ -39,10 +44,12 @@ describe("transformTrampoline", () => {
       n > 1 ? f_(n - 1, acc + 1) : acc;
     const f = (n: number) => f_(n, 0);
 
-    const ftr_: RecFunc<[number, number], number> = (n: number, acc: number) =>
-      n > 1 ? E.left([undefined, [n - 1, acc + 1]]) : E.right(acc);
+    const ftr_: TrampolinedORSR<[number, number], number> = (
+      n: number,
+      acc: number
+    ) => (n > 1 ? E.left([undefined, [n - 1, acc + 1]]) : E.right(acc));
 
-    const ftr = (n: number) => transformTrampoline(ftr_)(n, 0);
+    const ftr = (n: number) => trampolineORSR(ftr_)(n, 0);
 
     // depends on Node's stack size: `node --v8-options | grep -A1 "stack-size"`
     const n = 2 ** 16;
@@ -55,4 +62,65 @@ describe("transformTrampoline", () => {
       expect(ftr(n)).toBe(n - 1);
     }).not.toThrow();
   });
+});
+
+describe("trampolineOR", () => {
+  it("transforms trampolined standard form factorial correctly", () => {
+    // A recursive factorial function in a standard form
+    const f = (n: number): number => (n > 1 ? n * f(n - 1) : 1);
+
+    // A trampolined standard form factorial function
+    const ftr_: TrampolinedOR<[number], number> = (n) =>
+      n > 1 ? E.left([(x) => n * x, () => ftr_(n - 1)]) : E.right(1);
+    const ftr = trampolineOR(ftr_);
+
+    for (let i = 0; i < 10; i++) {
+      expect(ftr(i)).toBe(f(i));
+    }
+  });
+
+  it("transforms trampolined tail-call form factorial correctly", () => {
+    // A recursive factorial function in a tail-call form
+    const f_ = (n: number, acc: number): number =>
+      n > 1 ? f_(n - 1, acc * n) : acc;
+
+    const f = (n: number) => f_(n, 1);
+
+    // A trampolined tail-call form factorial function
+    const ftr_: TrampolinedOR<[number, number], number> = (n, acc) =>
+      n > 1 ? E.left([undefined, () => ftr_(n - 1, n * acc)]) : E.right(acc);
+
+    const ftr = (n: number) => trampolineOR(ftr_)(n, 1);
+
+    for (let i = 0; i < 10; i++) {
+      expect(ftr(i)).toBe(f(i));
+    }
+  });
+
+  it("transforms a stack-overflowing function into a not-overflowing one", () => {
+    const f_ = (n: number, acc: number): number =>
+      n > 1 ? f_(n - 1, acc + 1) : acc;
+    const f = (n: number) => f_(n, 0);
+
+    const ftr_: TrampolinedOR<[number, number], number> = (
+      n: number,
+      acc: number
+    ) =>
+      n > 1 ? E.left([undefined, () => ftr_(n - 1, acc + 1)]) : E.right(acc);
+
+    const ftr = (n: number) => trampolineOR(ftr_)(n, 0);
+
+    // depends on Node's stack size: `node --v8-options | grep -A1 "stack-size"`
+    const n = 2 ** 16;
+
+    expect(() => {
+      f(n);
+    }).toThrow();
+
+    expect(() => {
+      expect(ftr(n)).toBe(n - 1);
+    }).not.toThrow();
+  });
+
+  // TODO: mutually recursive test case for `transformOR`
 });
--- src/trampoline.ts
+++ src/trampoline.ts
@@ -5,74 +5,163 @@ import { pipe } from "fp-ts/lib/function";
 import Either = E.Either;
 
 /**
- * A result computing function for some step; binds step-specific
- * data (e.g. `x => n * x` for `fact(n) = n * fact(n-1)`, where `n > 1`)
+ * Trampolined once-recursive (only one recursive call inside)
+ * self-recursive (calls only itself) function type
  */
-type ResultStep<B> = (b: B) => B;
+namespace TrampolinedORSR_ {
+  /**
+   * A result computing function for some step; binds step-specific
+   * data (e.g. `x => n * x` for `fact(n) = n * fact(n-1)`, where `n > 1`)
+   */
+  type ResultCombinator<B> = (b: B) => B;
 
-/**
- * A final case is a value to be reduced through all result computing
- * functions for each step
- */
-type FinalCase<B> = B;
+  /**
+   * A final case is a value to be reduced through all result computing
+   * functions for each step
+   */
+  type FinalCase<B> = B;
 
-/**
- * Non-final case consists of
- * - an (optional) result computing function for this step
- *   (e.g. `x => n * x` for `fact(n) = n * fact(n-1)`, where `n > 1`);
- *   if not provided, will be assumed as an `x => x` identity function
- * - a required list of arguments for the next trampolined function call
- */
-type NonFinalCase<As, B> = [ResultStep<B> | undefined, As];
+  /**
+   * Non-final case consists of
+   * - an (optional) result computing function for this step
+   *   (e.g. `x => n * x` for `fact(n) = n * fact(n-1)`, where `n > 1`);
+   *   if not provided, will be assumed as an `x => x` identity function
+   * - a required list of arguments for the next trampolined function call
+   */
+  type NonFinalCase<NextCaseArgs extends any[], B> = [
+    ResultCombinator<B> | undefined,
+    NextCaseArgs
+  ];
 
-/**
- * A trampolined recursive function result type
- * - `left` is a non-final case (e.g. `fact(n) = n * fact(n-1)`, where `n > 1`)
- * - `right` is a final case (e.g. `fact(1) = 1`)
- */
-type RecFuncRes<As extends any, B> = Either<NonFinalCase<As, B>, FinalCase<B>>;
+  /**
+   * A trampolined recursive function result type
+   * - `left` is a non-final case (e.g. `fact(n) = n * fact(n-1)`, where `n > 1`)
+   * - `right` is a final case (e.g. `fact(1) = 1`)
+   */
+  type FuncRes<As extends any[], B> = Either<NonFinalCase<As, B>, FinalCase<B>>;
 
-/**
- * A trampolined recursive function type
- */
-export type RecFunc<As extends any[], B> = (...args: As) => RecFuncRes<As, B>;
+  /**
+   * A trampolined once-recursive function type
+   */
+  export type Func<As extends any[], B> = (...args: As) => FuncRes<As, B>;
+
+  export const trampoline_ =
+    <As extends any[], B>(f: Func<As, B>) =>
+    (...args: As): B => {
+      const stack: (ResultCombinator<B> | undefined)[] = [];
+      let r: B;
+      while (true) {
+        const next = f(...args);
+        if (E.isLeft(next)) {
+          stack.push(next.left[0]);
+          args = next.left[1];
+        } else {
+          r = next.right;
+          break;
+        }
+      }
+      return pipe(
+        stack,
+        A.reduceRight(r, (f, b) => (f ? f(b) : b))
+      );
+    };
+}
+
+export type TrampolinedORSR<As extends any[], B> = TrampolinedORSR_.Func<As, B>;
 
 /**
  * Transforms a trampolined once-recursive function into
  * a continuation stack-based function computing it
  *
- * *Note:* this function assumes that
+ * **Assumptions**
  * - `f` converges
  * - non-trampolined version of `f` calls only itself
+ * - `f` returns
+ *   - `Either.left` for a non-final case with a tuple
+ *     - an (optional) result combinator or undefined
+ *     - a list of arguments to pass to the next call
+ *   - `Either.right` for the final/base case
  *
  * @example
- * const fact: RecFunc<[number], number> =
+ * const fact_: TrampolinedORSR<[number], number> =
  *   (n) => n > 1 ? E.left([x => n * x, [n - 1]]) : E.right(1);
  *
- * const fact = itt(fact_);
+ * const fact = trampolineORSR(fact_);
  * fact(1) === 1;
  * fact(2) === 2;
  * fact(3) === 6;
  */
-export const transformTrampoline =
-  <As extends any[], B>(f: RecFunc<As, B>) =>
-  (...args: As): B => {
-    const stack: (ResultStep<B> | undefined)[] = [];
-    let r: B;
-    while (true) {
-      const next = f(...args);
-      if (E.isLeft(next)) {
-        stack.push(next.left[0]);
-        args = next.left[1];
-      } else {
-        r = next.right;
-        break;
-      }
-    }
-    return pipe(
-      stack,
-      A.reduceRight(r, (f, b) => (f ? f(b) : b))
-    );
-  };
+export const trampolineORSR = <As extends any[], B>(
+  f: TrampolinedORSR<As, B>
+): ((...args: As) => B) => TrampolinedORSR_.trampoline_(f);
 
-// TODO: create a trampolined function that returns a thunk
+/**
+ * Trampolined once-recursive function that uses thunks and optional
+ * result combinators to delay result calculation and execution
+ */
+namespace TrampolinedOR_ {
+  type ResultCombinator<B> = (x: B) => B;
+
+  type FinalCase<B> = B;
+
+  type NextCaseThunk<As extends any[], B> = () => FuncRes<As, B>;
+
+  type NonFinalCase<As extends any[], B> = [
+    ResultCombinator<B> | undefined,
+    NextCaseThunk<As, B>
+  ];
+
+  type FuncRes<As extends any[], B> = Either<NonFinalCase<As, B>, FinalCase<B>>;
+
+  export type Func<As extends any[], B> = (...args: As) => FuncRes<As, B>;
+
+  export const trampoline_ =
+    <As extends any[], B>(f: Func<As, B>) =>
+    (...args: As): B => {
+      const stack: (ResultCombinator<B> | undefined)[] = [];
+      let next: FuncRes<As, B> = f(...args);
+      let r: B;
+      while (true) {
+        if (E.isLeft(next)) {
+          stack.push(next.left[0]);
+          next = next.left[1]();
+        } else {
+          r = next.right;
+          break;
+        }
+      }
+      return pipe(
+        stack,
+        A.reduceRight(r, (f, b) => (f ? f(b) : b))
+      );
+    };
+}
+
+export type TrampolinedOR<As extends any[], B> = TrampolinedOR_.Func<As, B>;
+
+/**
+ * Transforms a trampolined once-recursive function into
+ * a continuation stack-based function computing it
+ *
+ * **Assumptions**
+ * - `f` converges
+ * - `f` returns
+ *   - `Either.left` for a non-final case with a tuple
+ *     - an (optional) result combinator
+ *     - a thunk returning `Either` for the next iteration
+ *   - `Either.right` for the final/base case
+ *
+ * @example
+ * const fact_: TrampolinedOR<[number], number> = (n) =>
+ *   n > 1 ? E.left([x => n * x, () => fact(n - 1)]) : E.right(n);
+ *
+ * const fact = trampolineOR(fact_);
+ * fact(1) === 1;
+ * fact(2) === 2;
+ * fact(3) === 6;
+ *
+ * // TODO: add an example for mutual recursion
+ */
+export const trampolineOR = <As extends any[], B>(
+  f: TrampolinedOR<As, B>
+): ((...args: As) => B) => TrampolinedOR_.trampoline_(f);
